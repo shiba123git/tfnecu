fc18f0aa5f6c896b66e21a02ca32b08ea3cd5cb1
shiba123git <shibaprasadm657@gmail.com>
2017-08-06 15:17:47 +0530

    shiba123git commit by shiba

---
+++ src/main/java/ae/co/pims/common/ModelConstant.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ae.co.pims.common;
+
+/**
+ * @author Babu
+ *
+ */
+public interface ModelConstant {
+
+	interface USER_LOGIN {
+		String USER_HOME_LOCATION_URL = "/secure/jsp/myHome.jsp?login_Unique_Transaction_id=%s&sessionId=%s";
+		String USER_NAME = "userName";
+		String PASSWORD = "passsword";
+	}
+
+}
--- src/main/java/ae/co/pims/web/LoginController.java
+++ src/main/java/ae/co/pims/web/LoginController.java
@@ -1,5 +1,50 @@
 package ae.co.pims.web;
 
-public class LoginController {
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ae.co.pims.common.ModelConstant;
+
+public class LoginController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1007870378736010547L;
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter(ModelConstant.USER_LOGIN.USER_NAME);
+		String password = request.getParameter(ModelConstant.USER_LOGIN.PASSWORD);
+		HttpSession session = request.getSession();
+		if (username.equals(password)) {
+			RequestDispatcher rd = request.getRequestDispatcher(
+					String.format(ModelConstant.USER_LOGIN.USER_HOME_LOCATION_URL, UUID.randomUUID(), session.getId()));
+			try {
+				rd.forward(request, response);
+			} catch (ServletException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
 
 }
