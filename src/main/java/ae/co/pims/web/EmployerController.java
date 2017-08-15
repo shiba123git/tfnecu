97b1f10e05cbfdc14e167bda15c8ea511ae2801e
shiba123git <shibaprasadm657@gmail.com>
2017-08-15 17:49:51 +0530

    shiba123git

--- src/main/java/ae/co/pims/web/EmployerController.java
+++ src/main/java/ae/co/pims/web/EmployerController.java
@@ -3,15 +3,30 @@
  */
 package ae.co.pims.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author Babu
  *
  */
+@Controller
 public class EmployerController {
 
-	
-	public EmployerController() {
-		
+	@RequestMapping(value = "/Login.spring", method = RequestMethod.POST)
+	public ModelAndView processCrediential(@RequestParam("username") String username, @RequestParam String password) {
+
+		String message = "Invalid Credientials";
+		if (null != username && null != password) {
+			if (username.equals(password)) {
+				message = "Welcome " + username + "!!!";
+			}
+		}
+		return new ModelAndView("result", "message", message);
+
 	}
 
 }
